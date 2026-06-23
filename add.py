96948cb71ad2ddb01015ba24f9339a2257dbf24b
garikapati-vijitha <garikapativijithachowdary2004@gmail.com>
2026-06-23 12:33:44 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,12 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 10+4
+14
+>>> 10-4
+6
+>>> 10*4
+40
+>>> 8/4
+2.0
+>>> print("vijitha")
+vijitha
