3c402ef34cdcec4aaca410611d08dc2e84e38696
garikapati-vijitha <garikapativijithachowdary2004@gmail.com>
2026-06-27 20:19:15 +0530

    Add files via upload

---
+++ indexing types.py
@@ -0,0 +1,52 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+# INDEXING
+# ACCESSING A STRING TO A VARIABLE
+a = "vijitha"
+a[0]
+'v'
+a[1]
+'i'
+a[2]
+'j'
+a[3]
+'i'
+a[4]
+'t'
+a[5]
+'h'
+a[6]
+'a'
+a[0]+a[1]+a[2]+a[3]+a[4]+a[5]+a[6]
+'vijitha'
+# POSITIVE INDEXING
+a="I AM LEARNING PYTHON COURSE"
+a[14]+a[15]+a[16]+a[17]+a[18]+a[19]
+'PYTHON'
+>>> a[5]+a[6]+a[7]+a[8]+a[9]
+'LEARN'
+>>> a[21]+a[22]+a[23]+a[24]+a[25]+a[26]
+'COURSE'
+>>> b="TIME IS VERY PRECIOUS"
+>>> b[13]+b[14]+b[15]+b[16]+b[17]+b[18]+b[19]+b[20]
+'PRECIOUS'
+>>> b[8]+b[9]+b[10]+b[11]
+'VERY'
+>>> b[0]+b[1]+b[2]+b[3]
+'TIME'
+>>> 
+>>> # NEGATIVE INDEXING
+>>> c = "simple is better than complex"
+>>> c[-29]+c[-28]+c[-27]+c[-26]+c[-25]+c[-24]
+'simple'
+>>> c[-12]+c[-11]+c[-10]+c[-9]
+'than'
+>>> c[-7]+c[-6]+c[-5]+c[-4]+c[-3]+c[-2]+c[-1]
+'complex'
+>>> c[-12]+c[-11]+c[-10]+c[-9]
+'than'
+>>> d="I LOVE PYTHON"
+>>> d[-6]+d[-5]+d[-4]+d[-3]+d[-2]+d[-1]
+'PYTHON'
+>>> d[-11]+d[-10]+d[-9]+d[-8]
+'LOVE'
---
+++ operators(Bitwise operator).py
@@ -0,0 +1,63 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+# BITWISE OPERATOR
+# AND OPERATOR
+a=9
+b=3
+a&b
+1
+del a
+del b
+a=8
+b=1
+a&b
+0
+bin(4)
+'0b100'
+bin(9)
+'0b1001'
+bin(2)
+'0b10'
+bin(6)
+'0b110'
+bin(0)
+'0b0'
+>>> # OR OPERATOR
+>>> a=9
+>>> b=8
+>>> a|b
+9
+>>> 
+>>> # NOT OPERATOR
+>>> # HERE WE USE ONLY 1 VALUE TO ASSIGN A NUMBER
+>>> a=3
+>>> ~a
+-4
+>>> # -(a+1) FORMULAE FOR NOT OPERATOR
+>>> 
+>>> 
+>>> #X-OR OPERATOR
+>>> A=3
+>>> B=6
+>>> A^B
+5
+>>> 
+>>> 
+>>> # LEFT SHIFT
+>>> # 0 SHOULD BE ADD ON THE RIGHT SIDE
+>>> A=2
+>>> A>>2
+0
+>>> B=6
+>>> B>>3
+0
+>>> B>>2
+1
+>>> B>>1
+3
+>>> 
+>>> #RIGHT SHIFT (ADD ZEROS ON LEFT SIDE SAME THE SAME NUMBER COUNT ON RIGHT SIDE)
+>>> 
+>>> a=8
+>>> a>>2
+2
